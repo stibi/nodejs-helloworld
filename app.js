8427a686abf55e95fdf7cd9c13ac47df6b64caa1
Martin Stiborsky <martin.stiborsky@gmail.com>
2016-12-16 00:40:08 +0100

    testing aws codepipeline

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
+let message = 'Hello world';
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(message)
 })
 
 app.listen(3000, function () {
