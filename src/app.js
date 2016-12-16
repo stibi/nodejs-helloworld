9733ad8f3aff1b87254f6a358dfc51cd3b49959b
Martin Stiborsky <martin.stiborsky@gmail.com>
2016-12-16 10:27:21 +0100

    testing repackaging the zip with lambda

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ let message = 'Hello world';
 // [01] 16/12/2016 01:12 Mam lambdu, ktera by mela byt schopna stahnout ZIP s kodem z Githubu a placnout ho na S3
 // [02] 16/12/2016 01:18 tak to by bylo, ted teda samotny build
 // [03] 16/12/2016 01:18 mam gulpfile a takovy pseudo build
+// [04] 17/12/2016 10:25 lambda by ted mela prebalit githubi zip, zrusit root adresar
 
 app.get('/', function (req, res) {
   res.send(message)
